aaee5ba5d2c8170d7c1a42f09d38e5791c7e7a4f
PRK <prk@prkspeaks.com>
2023-05-29 09:47:52 -0400

    Initiated main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number = rand() % 100; // Generate a random number between 0 and 99
+    printf("Random number: %d\n", number);
+    return 0;
+}
